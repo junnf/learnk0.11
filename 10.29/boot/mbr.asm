77fb92a2e8f42d01f237cf75587043e6d5d71f0f
junn <junnflow@gmail.com>
2016-10-30 23:29:48 +0800

    restart

---
+++ 10.29/boot/mbr.asm
@@ -0,0 +1,68 @@
+app_start equ 100
+SECTION mbr align=16 cstart=0x7c00
+mov ax,0
+mov ss,ax
+
+mov ax, [cs:phy_base]
+mov dx, [cs:phy_base + 0x02]
+mov bx 16
+div
+;商在ax,余数dx
+mov ds,ax
+mov es,ax
+
+read_hard_disk_0:                        ;从硬盘读取一个逻辑扇区
+                                         ;输入：DI:SI=起始逻辑扇区号
+                                         ;      DS:BX=目标缓冲区地址
+         push ax
+         push bx
+         push cx
+         push dx
+      
+         mov dx,0x1f2
+         mov al,1
+         out dx,al                       ;读取的扇区数
+
+         inc dx                          ;0x1f3
+         mov ax,si
+         out dx,al                       ;LBA地址7~0
+
+         inc dx                          ;0x1f4
+         mov al,ah
+         out dx,al                       ;LBA地址15~8
+
+         inc dx                          ;0x1f5
+         mov ax,di
+         out dx,al                       ;LBA地址23~16
+
+         inc dx                          ;0x1f6
+         mov al,0xe0                     ;LBA28模式，主盘
+         or al,ah                        ;LBA地址27~24
+         out dx,al
+
+         inc dx                          ;0x1f7
+         mov al,0x20                     ;读命令
+         out dx,al
+
+  .waits:
+         in al,dx
+         and al,0x88
+         cmp al,0x08
+         jnz .waits                      ;不忙，且硬盘已准备好数据传输 
+
+         mov cx,256                      ;总共要读取的字数
+         mov dx,0x1f0
+  .readw:
+         in ax,dx
+         mov [bx],ax
+         add bx,2
+         loop .readw
+
+         pop dx
+         pop cx
+         pop bx
+         pop ax
+      
+         ret
+
+phy_base dd 0x10000
